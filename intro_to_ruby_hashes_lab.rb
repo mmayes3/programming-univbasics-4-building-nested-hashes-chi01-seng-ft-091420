b5f7ebb74923aba9f48b20ad4ae160ac662d4297
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-31 00:38:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ end
 def monopoly_with_third_tier
   my_hash = {railroads:
   {pieces: 4, 
-  rent_in_dollars: {one_piece_owned: 25, }, 
+  rent_in_dollars: {one_piece_owned: 25, two_pieces_owned}, 
   names: {}
   }
     
